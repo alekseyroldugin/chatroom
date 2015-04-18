45de20245870bec7b0baa11e113738090ef38584
Aleksey Roldugin <rolduginaleksei@gmail.com>
2015-04-18 11:50:35 +0300

    Add devise views. Change user views and routs.rb for using devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,30 +12,32 @@ class UsersController < ApplicationController
   def show
   end
 
-  # GET /users/new
-  def new
-    @user = User.new
-  end
+  # Для создания нового пользователя используется devise
+  # # GET /users/new
+  # def new
+  #   @user = User.new
+  # end
 
   # GET /users/1/edit
   def edit
   end
 
-  # POST /users
-  # POST /users.json
-  def create
-    @user = User.new(user_params)
+  # Для создания нового пользователя используется devise
+  # # POST /users
+  # # POST /users.json
+  # def create
+  #   @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #   respond_to do |format|
+  #     if @user.save
+  #       format.html { redirect_to @user, notice: 'User was successfully created.' }
+  #       format.json { render :show, status: :created, location: @user }
+  #     else
+  #       format.html { render :new }
+  #       format.json { render json: @user.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
